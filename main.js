de0175cc9cd2e24f8d360003a28055099a11a745
Sabrina Paola <sabripaola1808@gmail.com>
2018-11-27 18:57:03 +0100

    fine jquery (aggiunto toggleClass)

---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    console.log("documento pronto")
+    $("#icon-menu").on("click",function(){
+        $("#menu2").toggleClass("display");
+    })
+})
+// toggleClass aggiunge e rimuove automaticamente
